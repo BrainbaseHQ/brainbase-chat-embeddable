a1c84554041312c13b49a9e9a5a29a574717779a
Nauxie <abhinav.tumu1@gmail.com>
2025-12-08 15:13:29 -0800

    Export all components

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+export { ChatWidget } from './ChatWidget';
+export { ChatContainer } from './ChatContainer';
+export { ChatHeader } from './ChatHeader';
+export { ChatToggleButton } from './ChatToggleButton';
+export { Message } from './Message';
+export { MessageList } from './MessageList';
+export { MessageInput } from './MessageInput';
+export { ToolCallDisplay } from './ToolCallDisplay';
+export { TypingIndicator } from './TypingIndicator';
+
+export type { ChatContainerProps } from './ChatContainer';
+export type { ChatHeaderProps } from './ChatHeader';
+export type { ChatToggleButtonProps } from './ChatToggleButton';
+export type { MessageProps } from './Message';
+export type { MessageListProps } from './MessageList';
+export type { MessageInputProps } from './MessageInput';
+export type { ToolCallDisplayProps } from './ToolCallDisplay';
+export type { TypingIndicatorProps } from './TypingIndicator';
+
